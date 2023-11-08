15e4c54a0e8610070f489c9e548e919e00005807
Ernest Badu <ernestjbadu@outlook.com>
2023-11-08 10:03:42 +0000

    chore: fix build

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -13,4 +13,12 @@ export default defineConfig({
 	sourcemap: true,
 	clean: true,
 	splitting: false,
+	banner: {
+		js: `
+// BANNER START
+const require = (await import("node:module")).createRequire(import.meta.url);
+const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+// BANNER END
+`,
+	},
 });

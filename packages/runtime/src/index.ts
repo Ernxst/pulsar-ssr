9af6a8eb720145b309112a9beaa6f3a89749d790
Ernest Badu <ernestjbadu@outlook.com>
2023-11-10 17:14:03 +0000

    chore: format code

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,3 +1,6 @@
 export { createPulsarRequest, sendPulsarResponse } from 'src/utils/http';
 export { transformPathToUrl } from 'src/utils/transform-path-to-url';
-export { createActionUrl, PULSAR_FORM_ACTIONS_METHOD } from 'src/utils/create-action-url';
+export {
+	createActionUrl,
+	PULSAR_FORM_ACTIONS_METHOD,
+} from 'src/utils/create-action-url';

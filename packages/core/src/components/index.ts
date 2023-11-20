3be09c2490c7ba0a4cadbad475a2d5763576d818
Ernest Badu <ernestjbadu@outlook.com>
2023-11-20 20:13:37 +0000

    chore: format code

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -3,8 +3,13 @@ export { Suspense } from 'hono/jsx/streaming';
 export { LiveReload } from './LiveReload';
 
 /**
- * We export these from here, rather than the JSX entry because they import 
+ * We export these from here, rather than the JSX entry because they import
  * Hono types. Importing anything from hono/jsx will overwrite our custom
  * JSX types for something
  */
-export type { Children, Component, ComponentProps, PropsWithChildren } from "./types"
+export type {
+	Children,
+	Component,
+	ComponentProps,
+	PropsWithChildren,
+} from './types';

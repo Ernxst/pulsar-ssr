aaf772f3257cdd9dafe1ad4ac212d7df18af72ac
Ernest Badu <ernestjbadu@outlook.com>
2023-11-20 19:45:07 +0000

    fix: make cloudflare worker adapter use correct adapter function

--- packages/vite-plugin-pulsar/src/adapters/cloudflare-workers.ts
+++ packages/vite-plugin-pulsar/src/adapters/cloudflare-workers.ts
@@ -3,7 +3,7 @@ import type { Adapter } from './types';
 export function cloudflare(): Adapter {
 	return {
 		name: 'pulsar-cloudflare-workers',
-		adapterFunction: 'createFetchRequestHandler',
+		adapterFunction: 'createCloudflareWorker',
 		createServer({ handler }) {
 			return `export default {
 				fetch: ${handler}

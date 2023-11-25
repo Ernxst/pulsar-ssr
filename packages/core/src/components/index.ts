c08d34bbb753bb3cf8877d02e607bb490a9fdfa0
Ernest Badu <ernestjbadu@outlook.com>
2023-11-25 11:09:58 +0000

    chore: format code

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,7 +1,10 @@
 export { Fragment, createContext, memo, useContext } from 'hono/jsx';
 export { Suspense } from 'hono/jsx/streaming';
 export { LiveReload } from './LiveReload';
-export { ScrollRestoration, type ScrollRestorationProps } from "./ScrollRestoration"
+export {
+	ScrollRestoration,
+	type ScrollRestorationProps,
+} from './ScrollRestoration';
 
 /**
  * We export these from here, rather than the JSX entry because they import
